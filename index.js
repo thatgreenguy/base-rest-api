daabb90aa64ec3dae203a4338d7be55fa835fd8a
Paul <thatgreenguy@techie.com>
2018-04-03 21:14:00 +0100

    Hook up code coverage

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 const CFG = require('./src/utils/cfg')
+const log = require('./src/utils/logger')
 
 function dostuff () {
   console.log(JSON.stringify(CFG))
+  log.info(JSON.stringify(CFG))
 }
 
 setTimeout(dostuff, 2000)
---
+++ src/utils/log.js
@@ -0,0 +1,2 @@
+'use strict'
+const winston = require 'winston'
