a9105d68a5777b7559e919be5b7fac472a83ea3e
Paul <thatgreenguy@techie.com>
2018-04-04 22:19:51 +0100

    Dockerise node app and mongo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const CFG = require('./src/utils/cfg')
 const log = require('./src/utils/logger')
 
 function dostuff () {
-  log.info(JSON.stringify(CFG))
+  log.info('Yep siree bob!' + JSON.stringify(CFG))
 }
 
 setInterval(dostuff, 2000)
